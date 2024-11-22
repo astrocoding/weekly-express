2e51ce4f0ab6896caaef1cfbc997fee5d647de39
astrocoding <zaenalalfian20@gmail.com>
2024-11-22 16:08:12 +0700

    chore: add env.example and comment

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,4 +6,8 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 
+    // Untuk logout kita hanya perlu menghapus token dari sisi klien
+    // Jika menggunakan JWT, tidak perlu melakukan apa-apa di server
+    // Jadi cukup beri tahu sisi klien untuk menghapus token
+
 module.exports = router;
