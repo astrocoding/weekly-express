dcde0be2520a9a277e1ce8c5e3804cc331e87995
astrocoding <zaenalalfian20@gmail.com>
2024-11-22 11:24:39 +0700

    fix: auth middleware for bearer token

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const authMiddleware = (req, res, next) => {
-    const token = req.headers['x-access-token'];
+    const token = req.headers['authorization']?.split(' ')[1];
     if (!token) return res.status(403).json({ message: 'No token provided!' });
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
